bc7caf9c93c7d97e69dc1881f66e8e7ffc9979c9
pageza <zap.freelance@gmail.com>
2022-02-19 17:32:28 -0700

    fleshing out routes

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var usersRouter = require('./routes/users');
+const usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
+
 
 var app = express();
 
@@ -17,24 +18,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req,res,next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  );
+  next();
+})
+app.use('/', usersRouter);
+app.use('/posts', postsRouter);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+app.listen(3030)
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -7,8 +7,22 @@ router.get('/', function(req, res, next) {
 })
 
 // TODO: Create new user
+
 // TODO: Get logged in user
+router.get('/:uname', (req,res,next) => {
+  // sequelize call to db to get information of the logged in user
+  console.log("this is the req ",req.params)
+  res.json({
+    'status': 'success'
+  })
+})
 // TODO: Get following users
+router.get('/:uname/followers', function(req, res, next) {
+  // sequelize call to db to get all the users that are following the currently logged in user
+})
 // TODO: Get followed users
+router.get( '/:uname/followed', (req,res,next) => {
+  // sequelize call to db to get all the users that the currently logged in user is following.
+})
 
 module.exports = router;
