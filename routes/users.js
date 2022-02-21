2dd773b47e60f540ad001c86cca034de1b78aac4
pageza <zap.freelance@gmail.com>
2022-02-21 15:57:05 -0700

    adding logic to controllers

--- routes/users.js
+++ routes/users.js
@@ -9,19 +9,13 @@ router.get('/', function(req, res) {
 
 // TODO: Create new user
 
-// TODO: Get logged in user
 router.get('/:uname', (req,res) => {
-  // sequelize call to db to get information of the logged in user
     users.getOneUser(req,res)
 })
-// TODO: Get following users
 router.get('/followers/:uname', function(req, res) {
-  // sequelize call to db to get all the users that are following the currently logged in user
     users.getFollowers(req,res)
 })
-// TODO: Get followed users
 router.get( '/followed/:uname', (req,res) => {
-  // sequelize call to db to get all the users that the currently logged in user is following.
     users.getFollowedUsers(req,res)
 })
 
