57f6ae5cf92f89107385c80a71f61378829a151a
pageza <zap.freelance@gmail.com>
2022-02-21 16:01:32 -0700

    adding logic to controllers

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,30 +1,25 @@
-// const db = require('./databaseController')
 const User = require('../models/User')
 
 module.exports = {
     getOneUser: async (req,res) => {
-        // db.createConnection
         const user = await User.findOne({
             where: { uname: req.params['uname']}
         })
         res.json(user)
     },
     getUsers: async (req,res) => {
-        // db.createConnection
         const users = await User.findAll({
             attributes: {exclude: ['id']}
         })
         res.json(users)
     },
     getFollowers: async (req,res) => {
-        // db.createConnection
         const followers = User.findAll({
             where: {}
         })
         res.json(followers)
     },
     getFollowedUsers: async (req,res) => {
-        // db.createConnection
         const followedUsers = User.findAll({
             where: {}
         })
