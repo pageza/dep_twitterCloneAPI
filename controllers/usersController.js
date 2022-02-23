a54e0a676f2db56dbabf6f5ae70c57eb9ec13b7e
pageza <zap.freelance@gmail.com>
2022-02-23 11:49:45 -0700

    sequelize working

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,29 +3,32 @@ const User = require('../models/User')
 module.exports = {
     createUser: async (req,res) => {
         const user = await User.create({
-            // map form fields to sequelize create func
+            // TODO: map form fields to sequelize create func
         })
     },
     getOneUser: async (req,res) => {
         const user = await User.findOne({
+            attributes: {exclude: ['id']},
             where: { uname: req.params['uname']}
         })
-        res.json({'status': 'working'})
+        res.json(user)
     },
     getUsers: async (req,res) => {
         const users = await User.findAll({
             attributes: {exclude: ['id']}
         })
-        res.json({'status': 'working'})
+        res.json(users)
     },
     getFollowers: async (req,res) => {
         const followers = User.findAll({
+            // TODO: add logic to DB for getting followers of logged in User
             where: {}
         })
         res.json({'status': 'working'})
     },
     getFollowedUsers: async (req,res) => {
         const followedUsers = User.findAll({
+            // TODO: add logic to DB for getting users followed by logged in User
             where: {}
         })
         res.json({'status': 'working'})
