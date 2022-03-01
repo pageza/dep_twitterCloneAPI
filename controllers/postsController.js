c27e08436b105b47bb2107f03d2f2b64d29074a9
pageza <zap.freelance@gmail.com>
2022-03-01 12:31:12 -0700

    working on query for getting Explore posts

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,4 +1,6 @@
+const { Op } = require('sequelize')
 const Post = require('../models/Post')
+const Follow = require('../models/Follow')
 
 module.exports = {
     createPost: async (req,res) => {
@@ -20,14 +22,28 @@ module.exports = {
             .then(post => res.json(post))
     },
     getAllPostsFromOneUser: async (req,res) => {
-
+        const posts = await Post.findAll({
+            where: {
+                userID: req.params['userID']
+            }
+        })
+            .then( posts => res.json(posts))
     },
     getAllPostsFromAllUsers: async (req,res) => {
         const posts = await Post.findAll()
         res.json(posts)
     },
     getExplorePosts: async (req,res) => {
-
+        // First I need to find all of the follows by this user
+        // const followed = await Follow.findAll({
+        //     where:
+        // })
+        // // then I need to find all query excluding those users
+        // const post = await Post.findAll({
+        //     where: {
+        //
+        //     }
+        // })
     },
     updatePost: async (req,res) => {
 
