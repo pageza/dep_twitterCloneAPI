1ed9b01d8b27802f146bbbaf19920e3780f6ae83
pageza <zap.freelance@gmail.com>
2022-03-02 09:54:34 -0700

    broke out routes into a separate module.

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,16 @@
 // TODO: export this to shorten the app.js file
 
-// const usersRouter = require('./routes/users');
-// const postsRouter = require('./routes/posts');
-// const commentsRouter = require('./routes/comments');
-// const likesRouter = require('./routes/likes')
-// const followsRouter = require('./routes/follows')
-// const app = require('../app')
-//
-// module.exports = {
-//     app.use('/api/user', usersRouter),
-//     app.use('/api/posts', postsRouter),
-//     app.use('/api/comments', commentsRouter),
-//     app.use('/api/likes', likesRouter),
-//     app.use('/api/follows', followsRouter)
-// }
+const usersRouter = require('./users');
+const postsRouter = require('./posts');
+const commentsRouter = require('./comments');
+const likesRouter = require('./likes')
+const followsRouter = require('./follows')
+const app = require('../app')
+
+module.exports = routes => {
+    app.use('/api/user', usersRouter),
+    app.use('/api/posts', postsRouter),
+    app.use('/api/comments', commentsRouter),
+    app.use('/api/likes', likesRouter),
+    app.use('/api/follows', followsRouter)
+}
